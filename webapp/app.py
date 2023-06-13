0c7afe614b0fb1ee987eab1adf4ffb2cd6474e6b
cdouadi <douadichakib@gmail.com>
2023-06-13 14:30:37 +0200

    edited streamlit files

--- webapp/app.py
+++ webapp/app.py
@@ -2,17 +2,18 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 import base64
+import time
 
 st.set_page_config(
     page_title="AntiWaste",
-    page_icon="🍅",
+    page_icon="🗑️",
     layout="wide"
 )
 
 st.write("## Antiwaste application")
-st.write("🥑 Try uploading an image. This code is open source and available [antiwaste](https://github.com/cdouadi/antiwaste) on GitHub. 🍅")
+st.write("This code is open source and available [antiwaste](https://github.com/cdouadi/antiwaste) on GitHub.")
 st.sidebar.write("## Upload :gear:")
-picture = st.sidebar.camera_input("Take a picture")
+picture = st.sidebar.camera_input("🥑 Take photo 📸")
 
 
 if picture:
@@ -28,9 +29,33 @@ def fix_image(upload):
 
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+my_upload = st.sidebar.file_uploader("🍍 Upload an image 🥕", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
     fix_image(upload=my_upload)
 
-st.download_button("Download fixed image", "fixed.png", "image/png")
+st.sidebar.write("\n\n\n")
+st.sidebar.download_button("Submit image", "fixed.png", "image/png")
+
+
+
+with st.spinner(text='In progress'):
+   time.sleep(5)
+   st.sidebar.success('Done')
+
+st.sidebar.error('Error message')
+st.sidebar.warning('Warning message')
+st.sidebar.info('Info message')
+st.sidebar.success('Success message')
+
+"""
+st.balloons()
+st.snow()
+st.exception(e)
+"""
+progress_text = "Operation in progress. Please wait."
+my_bar = st.sidebar.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1, text=progress_text)
