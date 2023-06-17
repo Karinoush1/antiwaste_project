7b2f2b38e98525110444b7e4221c99a0473c6974
cdouadi <douadichakib@gmail.com>
2023-06-17 16:02:44 +0200

    modified webapp file

--- webapp/streamlit_app.py
+++ webapp/streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 from PIL import Image
-from io import BytesIO
-import base64
-import time
 from pyzbar.pyzbar import decode
 import openfoodfacts
 import re
@@ -11,23 +8,33 @@ import os
 from translate import Translator
 import requests
 
-
+# streamlit page setup
 st.set_page_config(
     page_title="AntiWaste",
     page_icon="🗑️",
-    layout="centered", #"wide" 
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
+# Spoonacular api key -> heroku
+api = sp.API(os.environ["SPOONACULAR_API_KEY"])
+
+# translator
+translator= Translator(to_lang="fr")
+
+# main page
 st.write("## Antiwaste application")
 st.write("This code is open source and available [antiwaste](https://github.com/cdouadi/antiwaste) on GitHub.")
 st.sidebar.write("## Upload :gear:")
 
-#picture = st.sidebar.camera_input("🥑 Take photo 📸")
-#if picture:
-#    st.image(picture)
+# nombre de colonnes
+nb_col = 4
+col1, col2, col3, col4 = st.columns(nb_col)
 
 
+###############################
+
+# 
 def fix_image(upload):
     image = Image.open(upload)
     col1.write("Original Image :camera:")
@@ -46,50 +53,47 @@ def fix_image(upload):
                 st.write(barcode.type)
         st.sidebar.markdown("\n")
     return detectedBarcodes
-# nombre de colonnes
-nb_col = [1,1,1,10]
-col1, col2, col3, col4 = st.columns(nb_col)
-my_upload = st.sidebar.file_uploader("🍍 Upload an image 🥕", type=["png", "jpg", "jpeg"])
-
-st.sidebar.button("prediction")
-product = []
-if my_upload is not None:
-    detectedBarcodes = fix_image(upload=my_upload)
-    for barcode in detectedBarcodes: 
-        if barcode.data!="": 
-            st.write(barcode.data)
-            product.append(openfoodfacts.products.get_product(re.search(r'\d+', str(barcode.data)).group()))
-
-    st.write("list barcode : ",product)
-
-api = sp.API(os.environ["SPOONACULAR_API_KEY"])
-
-# search recipes by ingredients
-response = api.search_recipes_by_ingredients(ingredients="apple, flour, citron", number=nb_col)
-
-data = response.json()
-
-# translator
-translator= Translator(to_lang="fr")
 
 
-column = [col1, col2, col3, col4]
+def main():
+   
+    # 1. sidebar upload image (fruits, vegetables, product with barcode)
+    my_upload = st.sidebar.file_uploader("🍍 Upload an image (fruits or product with barcode) 🥕", type=["png", "jpg", "jpeg"])
+    
+    st.sidebar.button("prediction")
+    product = []
+    if my_upload is not None:
+        detectedBarcodes = fix_image(upload=my_upload)
+        for barcode in detectedBarcodes: 
+            if barcode.data!="": 
+                st.write(barcode.data)
+                product.append(openfoodfacts.products.get_product(re.search(r'\d+', str(barcode.data)).group()))
 
-for recipe, i in zip(data, range(0,nb_col,1)):
-    url = recipe['image']
-    # translation
-    translation = translator.translate(recipe['title'])
-    column[i].write(translation)
+        st.write("list barcode : ",product)
 
+    # 2. search recipes by ingredients - spoonacular api
+    ingredients="apple, flour, citron"
+    st.write("La liste des ingrédients : ",ingredients)
+    
+    response = api.search_recipes_by_ingredients(ingredients=ingredients, number=nb_col)
+    data = response.json()
 
-    response = requests.head(url)
-    if response.status_code == 200:
-        # des fois spoonacular n'a pas d'images.
-        #column[i].write(recipe['image'])
-        column[i].image(recipe['image'])
-    else:
-        column[i].write("💔 The image is not available 😢")
+    column = [col1, col2, col3, col4]
+    # 3. parcours des recettes et affichage d'une recette par colonne (col1,...)
+    for i,recipe in enumerate(data):
+        url = recipe['image']
+        # translation of title recipe
+        translation = translator.translate(recipe['title'])
+        column[i].write(translation)
 
-    i += 1
+        # check if the image is available (spoonacular api).
+        response = requests.head(url)
+        if response.status_code == 200:
+            # des fois spoonacular n'a pas d'images.
+            # column[i].write(recipe['image'])
+            column[i].image(recipe['image'])
+        else:
+            column[i].write("💔 The image is not available 😢")
 
-
+if __name__ == '__main__':
+    main()
